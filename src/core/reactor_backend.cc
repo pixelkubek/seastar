f7acaac72d5d9eaa5ee84244140cd5cd38b9d4be
MrD4rkne <marcinsz.pv@gmail.com>
2026-01-15 11:23:58 +0100

    perf: use IORING_SETUP_SINGLE_ISSUER

--- src/core/reactor_backend.cc
+++ src/core/reactor_backend.cc
@@ -1969,6 +1969,7 @@ namespace uring {
     try_create_attached_asymmetric_uring(int uring_fd, bool throw_on_error) {
         auto params = ::io_uring_params{};
         params.flags |= IORING_SETUP_ATTACH_WQ | IORING_SETUP_SQPOLL;
+        params.flags |= IORING_SETUP_SINGLE_ISSUER;
         params.wq_fd = uring_fd;
         return try_create_asymmetric_uring_impl(params, throw_on_error);
     }
@@ -1983,6 +1984,7 @@ namespace uring {
 
         auto params = ::io_uring_params{};
         params.flags |= IORING_SETUP_SQPOLL | IORING_SETUP_SQ_AFF;
+        params.flags |= IORING_SETUP_SINGLE_ISSUER;
         params.sq_thread_cpu = worker_cpu;
         params.sq_thread_idle = std::chrono::duration_cast<std::chrono::milliseconds>(POLLER_SLEEP_TIMEOUT).count();
 

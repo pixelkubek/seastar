2dfaa6c990f6e5fb91ff732b737f4a20a51599d2
witek-formanski <witekformanski@gmail.com>
2026-03-02 13:31:26 +0100

    Introduce reactor_backend_uring_base class
    This class will be a base for all reactor backends using io_uring.
    
    Co-authored-by: MrD4rkne <marcinsz.pv@gmail.com>
    Co-authored-by: mikolajbilski <mjbilski@gmail.com>
    Co-authored-by: pixelkubek <150136485+pixelkubek@users.noreply.github.com>
    Co-authored-by: witek-formanski <witekformanski@gmail.com>

--- include/seastar/core/internal/pollable_fd.hh
+++ include/seastar/core/internal/pollable_fd.hh
@@ -92,6 +92,7 @@ public:
 
     friend class reactor;
     friend class pollable_fd;
+    friend class reactor_backend_uring_base;
     friend class reactor_backend_uring;
 
     future<size_t> read_some(char* buffer, size_t size);
